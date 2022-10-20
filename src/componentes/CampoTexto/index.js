b70bcaff9bc1fbf5674fd0ad8f22e7ecc7df374b
Paulo Martins <pgsm5@hotmail.com>
2022-10-20 18:51:57 -0300

    Gerenciamento de estado com react (criando submit do formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,18 +1,16 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
     
-    const[valor, setValor] = useState('')
-    
     const aoDigitado = (evento) => {
-        setValor(evento.target.value)
-        console.log(evento.target.value)
+        props.aoAlterado(evento.target.value)
     }
+
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 }
