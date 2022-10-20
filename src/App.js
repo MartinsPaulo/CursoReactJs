16c9471d6af09147a50480f6151e16a698370eb7
Paulo Martins <pgsm5@hotmail.com>
2022-10-20 19:02:47 -0300

    passando estado do formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
 
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => setColaboradores([...colaboradores, colaborador])}/>
     </div>
   );
 }
